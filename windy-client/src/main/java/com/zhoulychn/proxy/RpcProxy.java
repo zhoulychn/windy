099c80647313b483e37e59cb6b64d4a5d801ae83
zhouly <zhoulychn@qq.com>
2019-06-25 11:14:20 +0800

    commit

--- windy-client/src/main/java/com/zhoulychn/proxy/RpcProxy.java
+++ windy-client/src/main/java/com/zhoulychn/proxy/RpcProxy.java
@@ -16,12 +16,20 @@ import java.util.UUID;
  * <p>
  * 接口代理对象。
  * <p>
- * Object proxy是代理对象实例。
+ * Object proxy是。
  */
 
 
 public class RpcProxy implements InvocationHandler {
 
+    /**
+     * 代理方法调用
+     * @param proxy 代理对象实例
+     * @param method 调用方法
+     * @param args 调用方法参数
+     * @return  结果
+     * @throws Throwable 异常
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Class<?> clazz = method.getDeclaringClass();
@@ -37,6 +45,12 @@ public class RpcProxy implements InvocationHandler {
         return response.getResult();
     }
 
+    /**
+     * 生成代理类
+     * @param clazz 类
+     * @param <T> 泛型
+     * @return 代理类
+     */
     public static <T> T buildProxy(Class<T> clazz) {
         return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new RpcProxy());
     }
